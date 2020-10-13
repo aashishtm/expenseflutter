60c1b5cc5208fb2106cca4a40299970d0fec3a6c
Aashish Thapa Magar <official4aashish@gmail.com>
2020-10-14 01:46:03 +0545

    Web version created too

---
+++ android/app/src/main/kotlin/com/example/expenseflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expenseflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Chart extends StatelessWidget {
+  
+  List<Map<String,Object>> get groupedTransactionValues{
+    return List.generate(7,(index) {
+      return{'day':'S', 'amount':5000};
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
+      child: Row(
+        children: [
 
+        ],
+      ),
     );
   }
 }
